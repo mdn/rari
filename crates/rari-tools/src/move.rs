a0f64fb30b06f65cf8f34251f970d969d0e480b3
Andi Pieper <apieper@mozilla.com>
2024-08-28 18:32:55 +0200

    fix some testing issues rot rari-tools

--- crates/rari-tools/src/move.rs
+++ crates/rari-tools/src/move.rs
@@ -1,11 +1,7 @@
 use std::str::FromStr;
 
 use rari_doc::{
-    build,
-    docs::{
-        doc,
-        page::{self, PageCategory},
-    },
+    docs::page::{self, PageCategory, PageLike},
     resolve::build_url,
 };
 use rari_types::locale::Locale;
@@ -16,12 +12,8 @@ pub fn r#move(
     old_slug: &str,
     new_slug: &str,
     locale: Option<&str>,
-    assume_yes: bool,
+    _assume_yes: bool,
 ) -> Result<(), ToolError> {
-    println!(
-        "Hello, world! {} {} {:?} {}",
-        old_slug, new_slug, locale, assume_yes
-    );
     validate_args(old_slug, new_slug)?;
     let locale = if let Some(l) = locale {
         Locale::from_str(l)?
@@ -31,7 +23,7 @@ pub fn r#move(
     // println!("Locale: {:?}", locale);
 
     // Make a dry run to give some feedback on what would be done
-    let changes = do_move(old_slug, new_slug, &locale, true)?;
+    let _changes = do_move(old_slug, new_slug, &locale, true)?;
 
     Ok(())
 }
@@ -40,21 +32,22 @@ fn do_move(
     old_slug: &str,
     new_slug: &str,
     locale: &Locale,
-    dry_run: bool,
+    _dry_run: bool,
 ) -> Result<(), ToolError> {
     let old_url = build_url(old_slug, locale, PageCategory::Doc);
     let doc = page::Page::page_from_url_path(&old_url)?;
-    let new_parent_slug = println!("doc: {:?}", doc);
-
+    let new_parent_slug = parent_slug(new_slug)?;
+    let real_old_slug = doc.slug();
+    println!("new_parent_slug: {new_parent_slug} real_old_slug: {real_old_slug}");
     Ok(())
 }
 
-fn parent_slug(slug: &str) -> Option<&str> {
+fn parent_slug(slug: &str) -> Result<&str, ToolError> {
     let slug = slug.trim_end_matches('/');
     if let Some(i) = slug.rfind('/') {
-        Some(&slug[..i])
+        Ok(&slug[..i])
     } else {
-        None
+        Err(ToolError::InvalidSlug("slug has no parent".to_string()))
     }
 }
 
@@ -84,6 +77,9 @@ fn validate_args(old_slug: &str, new_slug: &str) -> Result<(), ToolError> {
 
 #[cfg(test)]
 mod test {
+    use rari_types::globals::settings;
+    use std::env;
+
     use super::*;
 
     #[test]
@@ -93,14 +89,44 @@ mod test {
         assert!(validate_args("", "new").is_err());
         assert!(validate_args("old#", "new").is_err());
         assert!(validate_args("old", "new#").is_err());
-        println!("{:?}", validate_args("old", "new#"))
     }
 
     #[test]
     fn test_parent_slug() {
-        assert_eq!(parent_slug("a/b/c"), Some("a/b"));
-        assert_eq!(parent_slug("a/b"), Some("a"));
-        assert_eq!(parent_slug("a"), None);
-        assert_eq!(parent_slug("a/b/c/"), Some("a/b"));
+        assert_eq!(parent_slug("a/b/c").unwrap(), "a/b");
+        assert_eq!(parent_slug("a/b").unwrap(), "a");
+        assert!(parent_slug("a").is_err());
+        assert_eq!(parent_slug("a/b/c/").unwrap(), "a/b");
+    }
+
+    #[test]
+    fn test_do_move() {
+        // Test case where old_slug and new_slug are valid
+        let result = do_move(
+            "Web/API/AbortController",
+            "Web/API/AbortControllerAlternative",
+            &Locale::default(),
+            true,
+        );
+        println!("result: {:?}", result);
+        // assert!(do_move(
+        //     "Web/API/AbortController",
+        //     "Web/API/AbortControllerAlternative",
+        //     &Locale::default(),
+        //     true
+        // )
+        // .is_err());
+
+        // // Test case where old_slug is empty
+        // assert!(do_move("", "new", &Locale::default(), true).is_err());
+
+        // // Test case where new_slug is empty
+        // assert!(do_move("old", "", &Locale::default(), true).is_err());
+
+        // // Test case where old_slug contains '#'
+        // assert!(do_move("old#", "new", &Locale::default(), true).is_err());
+
+        // // Test case where new_slug contains '#'
+        // assert!(do_move("old", "new#", &Locale::default(), true).is_err());
     }
 }
