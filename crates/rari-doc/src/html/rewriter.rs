8d1b6725b71d73ee8166ca9ee0357fe5e40d617d
Vadim Makeev <vmakeev@mozilla.com>
2024-09-25 11:24:22 +0200

    Add a scrolling container around block math elements

--- crates/rari-doc/src/html/rewriter.rs
+++ crates/rari-doc/src/html/rewriter.rs
@@ -274,7 +274,7 @@ pub fn post_process_html<T: PageLike>(
             );
             Ok(())
         }),
-        element!("table", |el| {
+        element!("table, math[display=block]", |el| {
             el.before("<figure class=\"table-container\">", ContentType::Html);
             el.after("</figure>", ContentType::Html);
             Ok(())
