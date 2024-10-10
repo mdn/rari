d4398c8f523ca577e55f3868bd218220803c357b
Florian Dieminger <me@fiji-flo.de>
2024-10-10 09:53:27 +0200

    fix(templ): corrects ids from templates

--- crates/rari-doc/src/templ/templs/links/mathmlxref.rs
+++ crates/rari-doc/src/templ/templs/links/mathmlxref.rs
@@ -1,4 +1,5 @@
 use rari_templ_func::rari_f;
+use rari_utils::concat_strs;
 
 use crate::error::DocError;
 use crate::templ::api::RariApi;
@@ -6,12 +7,14 @@ use crate::templ::api::RariApi;
 #[rari_f]
 pub fn mathmlxref(element_name: String) -> Result<String, DocError> {
     let element_name = element_name.to_lowercase();
-    let display = format!("&lt;{element_name}&gt;");
-    let url = format!(
-        "/{}/docs/Web/MathML/Element/{}",
+    let display = concat_strs!("&lt;", element_name.as_str(), "&gt;");
+    let title = concat_strs!("<", element_name.as_str(), ">");
+    let url = concat_strs!(
+        "/",
         env.locale.as_url_str(),
-        element_name,
+        "/docs/Web/MathML/Element/",
+        element_name.as_str()
     );
 
-    RariApi::link(&url, None, Some(&display), true, Some(&display), false)
+    RariApi::link(&url, None, Some(&display), true, Some(&title), false)
 }
