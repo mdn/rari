be311ef84e1edc7da2d4cb50cfddaff3aed58b9a
Florian Dieminger <me@fiji-flo.de>
2024-11-25 18:01:59 +0100

    feat(rari-npm): use bin folder

--- rari-npm/lib/index.js
+++ rari-npm/lib/index.js
@@ -2,5 +2,5 @@ import { join } from "node:path";
 
 export const rariBin = join(
   import.meta.dirname,
-  `../binary/rari${process.platform === "win32" ? ".exe" : ""}`,
+  `../bin/rari${process.platform === "win32" ? ".exe" : ""}`,
 );
