8977887e1a199752b0f5c10f0604751852007915
Florian Dieminger <me@fiji-flo.de>
2025-01-21 17:28:25 +0100

    revert parser

--- crates/rari-doc/src/templ/parser.rs
+++ crates/rari-doc/src/templ/parser.rs
@@ -129,21 +129,7 @@ pub fn parse(input: &str) -> Result<Vec<Token>, DocError> {
         .into_inner()
         .filter_map(|t| match t.as_rule() {
             Rule::text => Some(Token::Text(t.into())),
-            Rule::macro_tag => {
-                let mut macro_roken: MacroToken = t.into();
-                // replace empty string args with None
-                // this is because we will use some judgements like `if let Some(arg) = arg`
-                // to determine whether the arg is empty or not
-                macro_roken.args = macro_roken
-                    .args
-                    .into_iter()
-                    .map(|arg| match arg {
-                        Some(Arg::String(s, _)) if s.is_empty() => None,
-                        _ => arg,
-                    })
-                    .collect();
-                Some(Token::Macro(macro_roken))
-            }
+            Rule::macro_tag => Some(Token::Macro(t.into())),
             _ => None,
         })
         .collect();
