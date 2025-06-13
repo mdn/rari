125014fb6bcd31a2ee8777277cbff043d0c50366
Claas Augner <caugner@mozilla.com>
2025-06-13 09:55:05 +0200

    chore(sidebar): remove unused toggle class

--- crates/rari-doc/src/helpers/subpages.rs
+++ crates/rari-doc/src/helpers/subpages.rs
@@ -268,7 +268,7 @@ pub fn list_sub_pages_flattened_grouped_internal(
     for (prefix, group) in grouped {
         let keep_group = group.len() > 2;
         if keep_group {
-            out.push_str("<li class=\"toggle\"><details><summary>");
+            out.push_str("<li><details><summary>");
             out.push_str(&html_escape::encode_safe(prefix));
             out.push_str("-*</summary><ol>");
         }
