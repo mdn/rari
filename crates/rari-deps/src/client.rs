c44bd1eb15209a33d7e8b115fe6fd7b47d46038a
A1lo <yin199909@aliyun.com>
2025-06-15 23:29:24 +0800

    feat(client): add GitHub token support for API requests (#239)
    * feat(client): add GitHub token support for API requests
    
    * format sources
    
    * remove unwrap and minor cleanup (#3)
    
    ---------
    
    Co-authored-by: Florian Dieminger <me@fiji-flo.de>

--- crates/rari-deps/src/client.rs
+++ crates/rari-deps/src/client.rs
@@ -1,9 +1,22 @@
 use reqwest::blocking::Response;
+use url::Url;
 
-pub fn get(url: impl AsRef<str>) -> reqwest::Result<Response> {
-    reqwest::blocking::ClientBuilder::new()
+use crate::error::DepsError;
+
+pub fn get(url: impl AsRef<str>) -> Result<Response, DepsError> {
+    let url = Url::parse(url.as_ref())?;
+    let mut req_builder = reqwest::blocking::ClientBuilder::new()
         .user_agent("mdn/rari")
         .build()?
-        .get(url.as_ref())
-        .send()
+        .get(url.as_ref());
+
+    // check if the URL's host is api.github.com
+    if url.host_str() == Some("api.github.com") {
+        // get the GitHub token from the environment
+        if let Ok(token) = std::env::var("GITHUB_TOKEN") {
+            req_builder = req_builder.bearer_auth(token);
+        }
+    }
+
+    Ok(req_builder.send()?)
 }
