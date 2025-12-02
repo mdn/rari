cf2818780d065d55bdcb4d1e6fa62f5514da4f81
Andi Pieper <apieper@mozilla.com>
2025-12-02 15:52:23 +0100

    walk the fiel system for css_ref

--- crates/rari-doc/src/templ/templs/css_ref.rs
+++ crates/rari-doc/src/templ/templs/css_ref.rs
@@ -9,217 +9,244 @@ use rari_utils::concat_strs;
 use regex::Regex;
 
 use crate::error::DocError;
-use crate::helpers::css_info::css_ref_data;
+use crate::helpers::subpages::{SubPagesSorter, get_sub_pages};
+use crate::pages::page::PageLike;
 use crate::templ::api::RariApi;
 
-static SYNTAX_RE: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r#"([\w-]+)\([\w\-<>,#+*?\[\]|\/ ]*\)|(@[\w-]+)"#).unwrap());
+// static SYNTAX_RE: LazyLock<Regex> =
+//     LazyLock::new(|| Regex::new(r#"([\w-]+)\([\w\-<>,#+*?\[\]|\/ ]*\)|(@[\w-]+)"#).unwrap());
 
-fn items_from_syntax(syntax: &str) -> Vec<Cow<'_, str>> {
-    SYNTAX_RE
-        .captures_iter(syntax)
-        .filter_map(|cap| cap.iter().skip(1).find(|c| c.is_some()))
-        .flatten()
-        .map(|i| {
-            let name = i.as_str();
-            if name.starts_with('@') {
-                Cow::Borrowed(name)
-            } else {
-                Cow::Owned(concat_strs!(name, "()"))
-            }
-        })
-        .collect()
-}
+// fn items_from_syntax(syntax: &str) -> Vec<Cow<'_, str>> {
+//     SYNTAX_RE
+//         .captures_iter(syntax)
+//         .filter_map(|cap| cap.iter().skip(1).find(|c| c.is_some()))
+//         .flatten()
+//         .map(|i| {
+//             let name = i.as_str();
+//             if name.starts_with('@') {
+//                 Cow::Borrowed(name)
+//             } else {
+//                 Cow::Owned(concat_strs!(name, "()"))
+//             }
+//         })
+//         .collect()
+// }
 
-static SKIPPED_ENTRIES: &[&str] = &[
-    "(-token",
-    ")-token",
-    "[-token",
-    "]-token",
-    "{-token",
-    "}-token",
-    "CDC-token",
-    "CDO-token",
-    " ",
-    "&",
-    "+",
-    "",
-    "||",
-    "~",
-    ":after",
-    ":before",
-    ":first-letter",
-    ":first-line",
-];
+// static SKIPPED_ENTRIES: &[&str] = &[
+//     "(-token",
+//     ")-token",
+//     "[-token",
+//     "]-token",
+//     "{-token",
+//     "}-token",
+//     "CDC-token",
+//     "CDO-token",
+//     " ",
+//     "&",
+//     "+",
+//     "",
+//     "||",
+//     "~",
+//     ":after",
+//     ":before",
+//     ":first-letter",
+//     ":first-line",
+// ];
 
 #[rari_f(register = "crate::Templ")]
 pub fn css_ref() -> Result<String, DocError> {
+    let pages = get_sub_pages(
+        "/en-US/docs/Web/CSS/Reference",
+        Some(2),
+        SubPagesSorter::IgnoringCSSPrefixes,
+    )?;
+
+    println!(
+        "pages: {:#?}",
+        pages.iter().map(|page| page.title()).collect::<Vec<_>>()
+    );
+
     let mut index = BTreeMap::<char, HashMap<Cow<'static, str>, Cow<'static, str>>>::new();
 
-    let prop_data = css_ref_data().properties.get("__global_scope__").unwrap();
-    for (prop_name, prop) in prop_data {
-        // Skip properties with legacy aliases, usually `-webkit-*` props
-        if prop.legacy_alias_of.is_some() {
-            continue;
-        }
-        let initial = initial_letter(prop_name);
+    for page in pages.iter()
+    // .filter(|page| page.page_type() == PageType::WebApiInterface)
+    {
+        let initial = initial_letter(page.title());
         let entry = index.entry(initial).or_default();
-        let (url, label) = adjust_output(
-            Cow::Owned(concat_strs!("Reference/Properties/", prop_name)),
-            Cow::Borrowed(prop_name),
-        );
+        let (url, label) = adjust_output(Cow::Borrowed(page.url()), Cow::Borrowed(page.title()));
         println!("============ CSSREF prop entry {url} {label}");
         entry.entry(url).or_insert(label);
     }
 
-    let type_data = css_ref_data().types.get("__global_scope__").unwrap();
-    for type_name in type_data.keys() {
-        let initial = initial_letter(type_name);
-        let entry = index.entry(initial).or_default();
+    // ========================================
 
-        if SKIPPED_ENTRIES.contains(&type_name.as_str()) {
-            continue;
-        }
-        let label = if type_name.starts_with('<') {
-            Cow::Borrowed(type_name.as_str())
-        } else {
-            Cow::Owned(concat_strs!("<", type_name, ">"))
-        };
-        let (url, label) = adjust_output(
-            Cow::Owned(concat_strs!("Reference/Values/", type_name)),
-            label,
-        );
-        println!("============ CSSREF type entry {url} {label}");
-        entry.entry(url).or_insert(label);
-    }
+    // let mut index = BTreeMap::<char, HashMap<Cow<'static, str>, Cow<'static, str>>>::new();
 
-    let atrule_data = css_ref_data().atrules.get("__global_scope__").unwrap();
-    for (at_rule_name, rule) in atrule_data {
-        let initial = initial_letter(at_rule_name);
-        let entry = index.entry(initial).or_default();
-        let (url, label) = adjust_output(
-            Cow::Owned(concat_strs!("Reference/At-rules/", at_rule_name)),
-            Cow::Borrowed(at_rule_name),
-        );
-        entry.entry(url).or_insert(label);
+    // let prop_data = css_ref_data().properties.get("__global_scope__").unwrap();
+    // for (prop_name, prop) in prop_data {
+    //     // Skip properties with legacy aliases, usually `-webkit-*` props
+    //     if prop.legacy_alias_of.is_some() {
+    //         continue;
+    //     }
+    //     let initial = initial_letter(prop_name);
+    //     let entry = index.entry(initial).or_default();
+    //     let (url, label) = adjust_output(
+    //         Cow::Owned(concat_strs!("Reference/Properties/", prop_name)),
+    //         Cow::Borrowed(prop_name),
+    //     );
+    //     println!("============ CSSREF prop entry {url} {label}");
+    //     entry.entry(url).or_insert(label);
+    // }
 
-        // main at rule
-        if let Some(syntax) = &rule.syntax {
-            for syntax_item in items_from_syntax(syntax) {
-                if &syntax_item == at_rule_name {
-                    println!(
-                        "============ CSSREF Skipping self-reference {syntax_item} | {at_rule_name}"
-                    );
-                    continue;
-                }
-                let initial = initial_letter(&syntax_item);
-                let entry = index.entry(initial).or_default();
+    // let type_data = css_ref_data().types.get("__global_scope__").unwrap();
+    // for type_name in type_data.keys() {
+    //     let initial = initial_letter(type_name);
+    //     let entry = index.entry(initial).or_default();
 
-                let url_path = Cow::Owned(concat_strs!(
-                    "Reference/At-rules/",
-                    at_rule_name.as_str(),
-                    "/",
-                    syntax_item.as_ref()
-                ));
-                let label = Cow::Owned(concat_strs!(syntax_item.as_ref(), " (", at_rule_name, ")"));
-                let (url, label) = adjust_output(url_path, label);
-                println!("============ CSSREF atrule  entry {url} {label}");
+    //     if SKIPPED_ENTRIES.contains(&type_name.as_str()) {
+    //         continue;
+    //     }
+    //     let label = if type_name.starts_with('<') {
+    //         Cow::Borrowed(type_name.as_str())
+    //     } else {
+    //         Cow::Owned(concat_strs!("<", type_name, ">"))
+    //     };
+    //     let (url, label) = adjust_output(
+    //         Cow::Owned(concat_strs!("Reference/Values/", type_name)),
+    //         label,
+    //     );
+    //     println!("============ CSSREF type entry {url} {label}");
+    //     entry.entry(url).or_insert(label);
+    // }
 
-                entry.entry(url).or_insert(label);
-            }
-        }
+    // let atrule_data = css_ref_data().atrules.get("__global_scope__").unwrap();
+    // for (at_rule_name, rule) in atrule_data {
+    //     let initial = initial_letter(at_rule_name);
+    //     let entry = index.entry(initial).or_default();
+    //     let (url, label) = adjust_output(
+    //         Cow::Owned(concat_strs!("Reference/At-rules/", at_rule_name)),
+    //         Cow::Borrowed(at_rule_name),
+    //     );
+    //     entry.entry(url).or_insert(label);
 
-        // at rule descriptors
-        for (d_name, descriptor) in &rule.descriptors {
-            let initial = initial_letter(d_name);
-            let entry = index.entry(initial).or_default();
+    //     // main at rule
+    //     if let Some(syntax) = &rule.syntax {
+    //         for syntax_item in items_from_syntax(syntax) {
+    //             if &syntax_item == at_rule_name {
+    //                 println!(
+    //                     "============ CSSREF Skipping self-reference {syntax_item} | {at_rule_name}"
+    //                 );
+    //                 continue;
+    //             }
+    //             let initial = initial_letter(&syntax_item);
+    //             let entry = index.entry(initial).or_default();
 
-            let url_path = Cow::Owned(concat_strs!(
-                "Reference/At-rules/",
-                at_rule_name,
-                "/",
-                &d_name
-            ));
-            let label = Cow::Owned(concat_strs!(&d_name, " (", at_rule_name, ")"));
-            let (url, label) = adjust_output(url_path, label);
-            entry.entry(url).or_insert(label);
+    //             let url_path = Cow::Owned(concat_strs!(
+    //                 "Reference/At-rules/",
+    //                 at_rule_name.as_str(),
+    //                 "/",
+    //                 syntax_item.as_ref()
+    //             ));
+    //             let label = Cow::Owned(concat_strs!(syntax_item.as_ref(), " (", at_rule_name, ")"));
+    //             let (url, label) = adjust_output(url_path, label);
+    //             println!("============ CSSREF atrule  entry {url} {label}");
 
-            if let Some(syntax) = &descriptor.syntax {
-                for syntax_item in items_from_syntax(syntax) {
-                    let initial = initial_letter(&syntax_item);
-                    let entry = index.entry(initial).or_default();
+    //             entry.entry(url).or_insert(label);
+    //         }
+    //     }
 
-                    if SKIPPED_ENTRIES.contains(&d_name.as_str()) {
-                        continue;
-                    }
-                    let url_path = Cow::Owned(concat_strs!(
-                        "Reference/At-rules/",
-                        at_rule_name,
-                        "/",
-                        &d_name,
-                        "#",
-                        syntax_item.as_ref()
-                    ));
-                    let (url, label) = adjust_output(url_path, syntax_item);
-                    println!(
-                        "============ CSSREF atrule descriptor entry {url} {label} | {d_name}"
-                    );
-                    entry.entry(url).or_insert(label);
-                }
-            }
-        }
-    }
+    //     // at rule descriptors
+    //     for (d_name, descriptor) in &rule.descriptors {
+    //         let initial = initial_letter(d_name);
+    //         let entry = index.entry(initial).or_default();
 
-    let selector_data = css_ref_data().selectors.get("__global_scope__").unwrap();
-    for selector_name in selector_data.keys() {
-        // Exclude basic selectors
-        if selector_name.contains(' ') {
-            continue;
-        }
-        if SKIPPED_ENTRIES.contains(&selector_name.as_str()) {
-            continue;
-        }
+    //         let url_path = Cow::Owned(concat_strs!(
+    //             "Reference/At-rules/",
+    //             at_rule_name,
+    //             "/",
+    //             &d_name
+    //         ));
+    //         let label = Cow::Owned(concat_strs!(&d_name, " (", at_rule_name, ")"));
+    //         let (url, label) = adjust_output(url_path, label);
+    //         entry.entry(url).or_insert(label);
 
-        let initial = initial_letter(selector_name);
-        let entry = index.entry(initial).or_default();
-        let (url, label) = adjust_output(
-            Cow::Owned(concat_strs!("Reference/Selectors/", selector_name)),
-            Cow::Borrowed(selector_name),
-        );
-        println!("============ CSSREF selector {url} {label}");
-        entry.entry(url).or_insert(label);
-    }
+    //         if let Some(syntax) = &descriptor.syntax {
+    //             for syntax_item in items_from_syntax(syntax) {
+    //                 let initial = initial_letter(&syntax_item);
+    //                 let entry = index.entry(initial).or_default();
 
-    let function_data = css_ref_data().functions.get("__global_scope__").unwrap();
-    for function_name in function_data.keys() {
-        if SKIPPED_ENTRIES.contains(&function_name.as_str()) {
-            continue;
-        }
+    //                 if SKIPPED_ENTRIES.contains(&d_name.as_str()) {
+    //                     continue;
+    //                 }
+    //                 let url_path = Cow::Owned(concat_strs!(
+    //                     "Reference/At-rules/",
+    //                     at_rule_name,
+    //                     "/",
+    //                     &d_name,
+    //                     "#",
+    //                     syntax_item.as_ref()
+    //                 ));
+    //                 let (url, label) = adjust_output(url_path, syntax_item);
+    //                 println!(
+    //                     "============ CSSREF atrule descriptor entry {url} {label} | {d_name}"
+    //                 );
+    //                 entry.entry(url).or_insert(label);
+    //             }
+    //         }
+    //     }
+    // }
 
-        let initial = initial_letter(function_name);
-        let entry = index.entry(initial).or_default();
-        let (url, label) = adjust_output(
-            Cow::Owned(concat_strs!("Reference/Values/", function_name)),
-            Cow::Borrowed(function_name),
-        );
-        println!("============ CSSREF function {url} {label}");
-        entry.entry(url).or_insert(label);
-    }
+    // let selector_data = css_ref_data().selectors.get("__global_scope__").unwrap();
+    // for selector_name in selector_data.keys() {
+    //     // Exclude basic selectors
+    //     if selector_name.contains(' ') {
+    //         continue;
+    //     }
+    //     if SKIPPED_ENTRIES.contains(&selector_name.as_str()) {
+    //         continue;
+    //     }
 
-    for (letter, url_path, label) in [
-        ('C', "counter", "<counter>"),
-        ('U', "url#The_url()_functional_notation", "url()"),
-        ('I', "inherit", "inherit"),
-        ('I', "initial", "initial"),
-        ('R', "revert", "revert"),
-        ('U', "unset", "unset"),
-        // ('V', "var", "var()"),
-    ] {
-        index
-            .entry(letter)
-            .or_default()
-            .insert(Cow::Borrowed(url_path), Cow::Borrowed(label));
-    }
+    //     let initial = initial_letter(selector_name);
+    //     let entry = index.entry(initial).or_default();
+    //     let (url, label) = adjust_output(
+    //         Cow::Owned(concat_strs!("Reference/Selectors/", selector_name)),
+    //         Cow::Borrowed(selector_name),
+    //     );
+    //     println!("============ CSSREF selector {url} {label}");
+    //     entry.entry(url).or_insert(label);
+    // }
+
+    // let function_data = css_ref_data().functions.get("__global_scope__").unwrap();
+    // for function_name in function_data.keys() {
+    //     if SKIPPED_ENTRIES.contains(&function_name.as_str()) {
+    //         continue;
+    //     }
+
+    //     let initial = initial_letter(function_name);
+    //     let entry = index.entry(initial).or_default();
+    //     let (url, label) = adjust_output(
+    //         Cow::Owned(concat_strs!("Reference/Values/", function_name)),
+    //         Cow::Borrowed(function_name),
+    //     );
+    //     println!("============ CSSREF function {url} {label}");
+    //     entry.entry(url).or_insert(label);
+    // }
+
+    // // Additional entries
+    // for (letter, url_path, label) in [
+    //     ('C', "Reference/Values/counter", "<counter>"),
+    //     ('U', "Reference/Values/url_value", "url()"),
+    //     ('I', "Reference/Values/initialinherit", "inherit"),
+    //     ('I', "Reference/Values/initial", "initial"),
+    //     ('R', "Reference/Values/revert", "revert"),
+    //     ('U', "Reference/Values/unset", "unset"),
+    //     ('-', "Reference/Properties/--*", "--*"),
+    // ] {
+    //     index
+    //         .entry(letter)
+    //         .or_default()
+    //         .insert(Cow::Borrowed(url_path), Cow::Borrowed(label));
+    // }
 
     let mut out = String::new();
     out.push_str(r#"<div class="index">"#);
@@ -234,7 +261,8 @@ pub fn css_ref() -> Result<String, DocError> {
             out.extend([
                 "<li>",
                 &RariApi::link(
-                    &concat_strs!("/Web/CSS/", url.as_ref()),
+                    // &concat_strs!("/Web/CSS/", url.as_ref()),
+                    &url,
                     Some(env.locale),
                     Some(&html_escape::encode_text(&label)),
                     true,
@@ -313,10 +341,11 @@ fn adjust_output<'a>(url: Cow<'a, str>, label: Cow<'a, str>) -> (Cow<'a, str>, C
         // | "swash()" => Cow::Owned(concat_strs!("@font-variant-alternates#", label.as_ref())),
 
         // Image
-        "image()" => Cow::Borrowed("image#The_image()_functional_notation"),
-        "image-set()" | "paint()" => {
-            Cow::Owned(concat_strs!("image/", label.trim_end_matches("()")))
-        }
+        "image()" => Cow::Borrowed("Reference/Values/image#The_image()_functional_notation"),
+        "image-set()" | "paint()" => Cow::Owned(concat_strs!(
+            "Reference/Values/image/",
+            label.trim_end_matches("()")
+        )),
 
         // Filter
         "blur()" | "brightness()" | "contrast()" | "drop-shadow()" | "grayscale()"
@@ -337,8 +366,8 @@ fn adjust_output<'a>(url: Cow<'a, str>, label: Cow<'a, str>) -> (Cow<'a, str>, C
         )),
 
         // Colors
-        "rgba()" => return (Cow::Borrowed("color_value/rgb"), label),
-        "hsla()" => return (Cow::Borrowed("color_value/hsl"), label),
+        "rgba()" => return (Cow::Borrowed("Reference/Valuescolor_value/rgb"), label),
+        "hsla()" => return (Cow::Borrowed("Reference/Values/color_value/hsl"), label),
         "rgb()" | "hsl()" | "hwb()" | "lab()" | "lch()" | "light-dark()" | "oklab()"
         | "oklch()" => Cow::Owned(concat_strs!(
             "Reference/Values/color_value/",
