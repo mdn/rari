9f41a9cee8a9588fca8f88e4cb596f2fb6e16622
Claas Augner <caugner@mozilla.com>
2026-05-13 12:09:55 +0200

    fix(quick_links_with_subpages): accept slug without locale
    Previously, this triggered flaws like this:
    
    ```
    fixed legacy url: Learn/Common_questions -> /es/docs/Learn/Common_questions
    ```

--- crates/rari-doc/src/templ/templs/quick_links_with_subpages.rs
+++ crates/rari-doc/src/templ/templs/quick_links_with_subpages.rs
@@ -3,12 +3,18 @@ use rari_types::{AnyArg, Arg};
 
 use super::listsubpages::listsubpages;
 use crate::error::DocError;
-use crate::templ::legacy::fix_broken_legacy_url;
 
 /// List sub pages
 #[rari_f(register = "crate::Templ")]
 pub fn quicklinkswithsubpages(url: Option<String>) -> Result<String, DocError> {
-    let url = url.map(|s| fix_broken_legacy_url(&s, env.locale).to_string());
+    let prefix = format!("/{}/docs", env.locale.as_url_str());
+    let url = url.map(|s| {
+        if s.starts_with(&prefix) {
+            s
+        } else {
+            format!("{prefix}/{}", s.trim_start_matches('/'))
+        }
+    });
     listsubpages(
         env,
         url,
